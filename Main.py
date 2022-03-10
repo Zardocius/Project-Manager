fec7a75a6bdbf1bf5bba876720741dbf1fa73156
Zardo <oziriz9@gmail.com>
2022-03-10 11:35:59 +0200

    main

---
+++ Main.py
@@ -0,0 +1,71 @@
+import time
+import os
+
+directory = os.getcwd()
+
+def option_1():
+    os.mkdir(option1)
+    os.chdir(option1)
+    content()
+
+def option_2():
+    os.mkdir(option2a + ' - ' + opinio2b)
+    os.chdir(option2a + ' - ' + opinio2b)
+    content()
+
+def content():
+    os.mkdir ('Textures')
+    os.mkdir ('Exports')
+    os.mkdir ('Images')
+    os.mkdir ('Documents')
+    os.mkdir ('Alphas')
+    os.mkdir ('Renders')
+
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
+logo = """
+  _____           _           _     __  __                                     ____  _____  
+ |  __ \         (_)         | |   |  \/  |                                   |___ \|  __ \ 
+ | |__) | __ ___  _  ___  ___| |_  | \  / | __ _ _ __   __ _  __ _  ___ _ __    __) | |  | |
+ |  ___/ '__/ _ \| |/ _ \/ __| __| | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|  |__ <| |  | |
+ | |   | | | (_) | |  __/ (__| |_  | |  | | (_| | | | | (_| | (_| |  __/ |     ___) | |__| |
+ |_|   |_|  \___/| |\___|\___|\__| |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|    |____/|_____/ 
+                _/ |                                          __/ |                         
+               |__/                                          |___/                          
+"""
+
+while True:
+    print (logo)
+    print ('To whom this shall be done? Me/Someone: ')
+
+    whom = input()
+
+    whoms = ['me','Me']
+    whomst = ['someone','Someone']
+    whomstdve = ['me','Me','someone','Someone']
+
+    if whom in whoms:
+        clearConsole()
+        print ('What shall be done?: ')
+        option1 = input()
+        option_1()
+        break
+
+    if whom in whomst:
+        clearConsole()
+        print ('Who Poor soul has offered their soul for such service?: ')
+        option2a = input()
+        clearConsole()
+        print ('What Shall be done?: ')
+        opinio2b = input()
+        option_2()
+        break
+
+    if whom not in whomstdve:
+        clearConsole()
+        print('You mere mortal not understand these simplest rules? Try again!')
+        time.sleep(2)
+        clearConsole()
+        continue
+
+
